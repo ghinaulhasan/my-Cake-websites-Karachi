ecef00c56d1d8c3ed02240a90ead6de1eb4da4fd
Syed Ghina-ul-Hassan <ffsgamerytgameryt@gmail.com>
2025-02-07 21:49:35 +0500

    second data commit

---
+++ html.js
@@ -0,0 +1,19 @@
+
+
+
+
+document.getElementById("exploreMenuButton").addEventListener("click", function() {
+    document.getElementById("cakes").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+// JavaScript to scroll to the footer when the "Call Now" button is clicked
+document.getElementById("callNowButton").addEventListener("click", function() {
+    // Scroll to the footer section smoothly
+    document.getElementById("footer").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+
